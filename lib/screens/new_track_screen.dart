403810ee8a40cc07aa5242fb9f36e27af685776e
DiegoIturra <diego269707@gmail.com>
2026-02-04 16:55:23 -0300

    feat: initial version of new track screen with three input text and two buttons, one for add a new participants (without functionality yet) , and the other for finish and return to the main screen

--- lib/screens/new_track_screen.dart
+++ lib/screens/new_track_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:split_track/screens/screens.dart';
 
 class NewTrackScreen extends StatelessWidget {
   const NewTrackScreen({super.key});
@@ -9,6 +10,72 @@ class NewTrackScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("New Track", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.indigo,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsetsGeometry.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  child: TextFormField(
+                    textCapitalization: TextCapitalization.words,
+                    decoration: const InputDecoration(
+                      labelText: 'Nombre',
+                      hintText: 'Ingresa un nombre',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        debugPrint('Added a new participant');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.green),
+                        foregroundColor: WidgetStateProperty.all(Colors.white),
+                      ),
+                      child: const Text("Add New Participant"),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        debugPrint('Finish');
+                        final route = MaterialPageRoute(
+                          builder: (context) => const TrackListScreen(),
+                        );
+                        Navigator.push(context, route);
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.red),
+                        foregroundColor: WidgetStateProperty.all(Colors.white),
+                      ),
+                      child: const Text("Finish"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
