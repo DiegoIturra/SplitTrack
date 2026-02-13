2d7b77ec07bd09d4101c4326efdb8ddc04e3650b
Diego Iturra <diturra@dparadig.com>
2026-02-13 13:36:59 -0300

    feat: create an abstract class to define the names of the differents routes across the app

---
+++ lib/routes/route_names.dart
@@ -0,0 +1,8 @@
+abstract class RouteNames {
+  static const expenseList = 'expense_list';
+  static const newExpense = 'new_expense';
+  static const iconSelector = 'icon_selector';
+  static const newtrack = 'new_track';
+
+  const RouteNames._();
+}
